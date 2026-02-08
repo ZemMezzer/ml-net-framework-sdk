945711548157422251f52a8899e4a69bc4c64401
ZemMezzer <drstive121319@gmail.com>
2026-02-08 22:04:58 +0100

    grammar bool

---
+++ MLSDK/src/Data/Grammar/Types/GrammarBool.cs
@@ -0,0 +1,6 @@
+﻿namespace MLAgentSDK.Data.Grammar.Types;
+
+public class GrammarBool : GrammarType
+{
+    public GrammarBool(string name, bool isRequired) : base(name, SchemaType.Boolean, isRequired) { }
+}
